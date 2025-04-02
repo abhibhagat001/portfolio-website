7a1b855b5c3d6763918ce1e965dd71d4b7415439
Abhishek Bhagat <abhibhagat111213@gmail.com>
2025-04-02 17:37:48 +0530

    repo initialisation

---
+++ README.md
@@ -0,0 +1,45 @@
+## 🚀Portfolio Website
+
+- This is a personal portfolio website built using HTML, CSS, JavaScript, and Bootstrap.
+
+## ✨Features
+
+📱 Responsive design using Bootstrap.
+🔄 Smooth scrolling animations.
+🧭 Interactive navigation bar.
+
+## 📂 Folder Structure
+
+Portfolio/
+│── assets/               # Images, SVG icons,logos
+│── css/                  # Stylesheets
+│── js/                   # JavaScript files
+|── libs                  # compiled bootstrap and ScrollReveal library
+│── index.html            # Main HTML file
+│── README.md             # Project documentation
+
+
+## 🛠 Technologies Used
+
+🏗  HTML5
+🎨 CSS3 (including Bootstrap)
+⚙️ JavaScript
+
+
+## 📥 Installation
+
+📌 Clone the repository:
+    - git clone https://github.com/yourusername/portfolio.git
+
+📂 Navigate to the project folder:
+    - cd portfolio
+
+🌐 Open index.html in your browser.
+
+## Live Demo
+
+- Click here to view the live version
+
+## Contact
+
+- For any inquiries, feel free to reach out at 📩abhibhagat111213@gmail.com
---
+++ js/index.js
@@ -0,0 +1,68 @@
+const sr = ScrollReveal({
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    reset: true,
+  });
+   
+  sr.reveal(".profile-pic", { delay: 200, origin: "left" });
+  sr.reveal("#intro-text", { delay: 400, origin: "bottom" });
+  sr.reveal("#contact-btn", { delay: 600, origin: "bottom" });
+  sr.reveal(".about-heading", { delay: 200, origin: "top" });
+  sr.reveal(".about-img", { delay: 200, origin: "left" });
+  sr.reveal(".fa-arrow-right", { delay: 200, origin: "left" });
+  sr.reveal(".fa-arrow-right", { delay: 200, origin: "left" });
+  sr.reveal(".work .heading", { delay: 300, origin: "left" });
+  sr.reveal(".work-description", { delay: 300, origin: "left" });
+  sr.reveal(".bullet-point", { delay: 300, origin: "left" });
+   
+  sr.reveal(".grid-item", {
+    origin: "bottom",
+    distance: "29px",
+    delay: 200,
+    interval: 100,
+    opacity: 0,
+  });
+   
+  sr.reveal(".company-logo", {
+    rotate: { x: 180, y: 0, z: 0 },
+    delay: 250,
+    opacity: 0,
+  });
+   
+  sr.reveal(".experience-description", { delay: 200, origin: "left" });
+   
+  sr.reveal(".project", {
+    origin: "bottom",
+    distance: "50px",
+    delay: 200,
+    interval: 200,
+    opacity: 0,
+  });
+   
+  sr.reveal(".experience-heading", {
+    delay: 200,
+    origin: "top",
+  });
+   
+  sr.reveal(".project-heading", {
+    delay: 200,
+    origin: "top",
+  });
+   
+  sr.reveal(".contact-text,.tagline", {
+    origin: "bottom",
+    distance: "50px",
+    delay: 200,
+    opacity: 0,
+  });
+   
+  sr.reveal(".social-links", {
+    origin: "bottom",
+    distance: "40px",
+    delay: 200,
+    opacity: 0,
+  });
+   
+   
+  
